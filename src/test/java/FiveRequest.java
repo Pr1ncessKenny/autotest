a107fa8bf8563cc56f4daa223fc5e8e0aa102219
AntonYakushenko <mnbv0@rambler.ru>
2021-08-09 20:03:51 +0400

    1.Добавлены запросы с другим текстом. 2.добавлены комменты к каждому тесту.

--- src/test/java/FiveRequest.java
+++ src/test/java/FiveRequest.java
@@ -18,6 +18,8 @@ public class FiveRequest {
         driver = new ChromeDriver();
     }
 
+
+    //карта мап купить
     @Test
     public void firstTest() throws InterruptedException {
         driver.get(SITE_URL);
@@ -32,6 +34,71 @@ public class FiveRequest {
         WebElement ttcsite = driver.findElement(By.partialLinkText("ttc.com.ru"));
         ttcsite.click();
     }
+
+    //карта мап купить спб
+    @Test
+    public void secondTest() throws InterruptedException {
+        driver.get(SITE_URL);
+
+        WebElement username = driver.findElement(By.xpath("//input[@tabindex='2']"));
+        Thread.sleep(5000);
+
+        username.sendKeys("карта мап купить спб", Keys.ENTER);
+
+        Thread.sleep(5000);
+
+        WebElement ttcsite = driver.findElement(By.partialLinkText("ttc.com.ru"));
+        ttcsite.click();
+    }
+
+    //карточка мап купить
+    @Test
+    public void thirdTest() throws InterruptedException {
+        driver.get(SITE_URL);
+
+        WebElement username = driver.findElement(By.xpath("//input[@tabindex='2']"));
+        Thread.sleep(5000);
+
+        username.sendKeys("карточка мап купить", Keys.ENTER);
+
+        Thread.sleep(5000);
+
+        WebElement ttcsite = driver.findElement(By.partialLinkText("ttc.com.ru"));
+        ttcsite.click();
+    }
+
+    //карточка мап купить спб
+    @Test
+    public void fourthTest() throws InterruptedException {
+        driver.get(SITE_URL);
+
+        WebElement username = driver.findElement(By.xpath("//input[@tabindex='2']"));
+        Thread.sleep(5000);
+
+        username.sendKeys("карточка мап купить спб", Keys.ENTER);
+
+        Thread.sleep(5000);
+
+        WebElement ttcsite = driver.findElement(By.partialLinkText("ttc.com.ru"));
+        ttcsite.click();
+    }
+
+    //карта допуска к международным перевозкам купить
+    @Test
+    public void fithTest() throws InterruptedException {
+        driver.get(SITE_URL);
+
+        WebElement username = driver.findElement(By.xpath("//input[@tabindex='2']"));
+        Thread.sleep(5000);
+
+        username.sendKeys("карта допуска к международным перевозкам купить", Keys.ENTER);
+
+        Thread.sleep(5000);
+
+        WebElement ttcsite = driver.findElement(By.partialLinkText("ttc.com.ru"));
+        ttcsite.click();
+    }
 }
+
 
 
